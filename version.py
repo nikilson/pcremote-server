5f762905ddd6f2211d1cc6c2fcb650e48fce61e1
Rinaldo Nikilson <nikilson30@gmail.com>
2022-11-04 18:44:45 +0530

    version file modified

--- version.py
+++ version.py
@@ -1,7 +1,7 @@
 VSVersionInfo(
   ffi=FixedFileInfo(
-    filevers=(1, 0, 3, 5),
-    prodvers=(1, 0, 3, 5),
+    filevers=(1, 0, 3, 6),
+    prodvers=(1, 0, 3, 6),
     mask=0x3f,
     flags=0x0,
     OS=0x40004,
@@ -16,12 +16,12 @@ VSVersionInfo(
         u'040904B0',
         [StringStruct(u'CompanyName', u'Rinaldo Nikilson'),
         StringStruct(u'FileDescription', u'Remotifier Server'),
-        StringStruct(u'FileVersion', u'1.0.3.5'),
+        StringStruct(u'FileVersion', u'1.0.3.6'),
         StringStruct(u'InternalName', u'cmd'),
         StringStruct(u'LegalCopyright', u'\xa9 Rinaldo Nikilson. All rights reserved.'),
         StringStruct(u'OriginalFilename', u'Remotifier.exe'),
         StringStruct(u'ProductName', u'Remotifier\xae Server\xae Software'),
-        StringStruct(u'ProductVersion', u'1.0.3.5')])
+        StringStruct(u'ProductVersion', u'1.0.3.6')])
       ]), 
     VarFileInfo([VarStruct(u'Translation', [1033, 1200])])
   ]
